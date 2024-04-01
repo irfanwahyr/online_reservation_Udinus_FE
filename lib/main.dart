0d12c98bc67a3eceee054502615ca7341cb1bd84
irfan <iramadhani679@gmail.com>
2024-04-01 14:15:02 +0700

    add logic sharedpreferences

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:kp2024/pages/admin/daftarPengguna.dart';
 import 'package:kp2024/pages/admin/homePageAdmin.dart';
 import 'package:kp2024/pages/admin/kontenDaftarMataKuliah/jadwalHari.dart';
 import 'package:kp2024/pages/detailLab/DetailLaboratorium.dart';
-
 import 'package:kp2024/pages/homePage.dart';
 import 'package:kp2024/pages/laboratorium/labDekstop.dart';
 import 'package:kp2024/pages/laboratorium/labMultimedia.dart';
@@ -19,8 +18,11 @@ import 'package:kp2024/pages/user/reservasiPage/berhasilSubmit.dart';
 import 'package:kp2024/pages/user/reservasiPage/keperluan.dart';
 import 'package:kp2024/pages/user/reservasiPage/reservasi.dart';
 import 'package:kp2024/pages/user/reservasiPage/riwayatReservasi.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main(List<String> args) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.clear();
   runApp(MyApp());
 }
 
@@ -32,6 +34,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
